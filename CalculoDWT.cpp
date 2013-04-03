4cbab5fa74fd12837dcef8daa3027d3f75bebe48
mars0 <mars0>
2013-04-03 14:02:37 +0200

    No podemos usar la clase Pixel en nuestro caso, porque trabajamos con el formato YCbCr 4:2:0. Es decir no hay tres componentes per píxel, sino hay sólo 1,5. Por eso he cambiado el fichero CalculoDWT para que no use la clase Pixel.

--- CalculoDWT.cpp
+++ CalculoDWT.cpp
@@ -12,10 +12,10 @@ float __inline **malloc_2d(int ancho, int alto) {
   return p;
 }
 
-Pixel aplica_kernel_f(int x, int y, int ancho, float *Y420, float *Cb420, float *Cr420, bool es_par, bool inverso = false) {
+/*Pixel aplica_kernel_f(int x, int y, int ancho, float *Y420, float *Cb420, float *Cr420, bool es_par, bool inverso = false) {
   Pixel po(Y420[x], Cr420[x], Cb420[x]);
   Pixel p[2];
-  if (es_par) { /* Es paso bajo! */
+  if (es_par) { // Es paso bajo! 
     po *= pbf[0];
     for (int j = 1; j < 5; j++) {
        if (x - j < 0)
@@ -28,7 +28,7 @@ Pixel aplica_kernel_f(int x, int y, int ancho, float *Y420, float *Cb420, float 
           p[1].set(Y420[x + j], Cb420[x + j], Cr420[x + j]);
        po += p[0] * pbf[j] + p[1] * pbf[j];
     }
-  } else { /* No es par... Es pasa alto! */
+  } else { // No es par... Es pasa alto! 
     po *= paf[0];
     for (int j = 0; j < 4; j++) {
        if (x - j < 0)
@@ -58,7 +58,56 @@ void DWT_f_filas(int ancho, int alto, float **Y420, float **Cb420, float **Cr420
       _Cr420[y][x] = p.Cr;
     }
 }
-//void DWT_f_columnas
+//void DWT_f_columnas */
+
+float aplica_kernel_f(int x, int y, int ancho, float *comp, bool es_par, bool inverso = false) {
+  float po = comp[x];
+  float p[2];
+  if (es_par) { // Es paso bajo!
+    po *= pbf[0];
+    for (int j = 1; j < 5; j++) {
+      if (x - j < 0)
+        p[0] = comp[x + j];
+      else
+        p[0] = comp[x - j];
+      if (x + j > ancho)
+        p[1] = comp[x - j];
+      else
+        p[1] = comp[x + j];
+      po += p[0] * pbf[j] + p[1] * pbf[j];
+    }
+  } else { // No es par... Es pasa alto!
+    po *= paf[0];
+    for (int j = 0; j < 4; j++) {
+      if (x - j < 0)
+        p[0] = comp[x + j];
+      else
+        p[0] = comp[x - j];
+      if (x + j > ancho)
+        p[1] = comp[x - j];
+      else
+        p[1] = comp[x + j];
+      po += p[0] * paf[j] + p[1] * paf[j];
+    }
+  }
+  return po;
+}
+
+void DWT_f_filas(int ancho, int alto, float **Y420, float **Cb420, float **Cr420) {
+  float **_Y420 = malloc_2d(ancho, alto);
+  float **_Cb420 = malloc_2d(ancho/2, alto/2);
+  float **_Cr420 = malloc_2d(ancho/2, alto/2);
+
+  for (int y = 0; y < alto; y++)
+    for (int x = 0; x < ancho; x++) {
+      _Y420[y][x] = aplica_kernel_f(x, y, ancho, Y420[y], (x % 2) == 0);
+      if (y < (alto/2) && x < (ancho/2)) {
+        _Cb420[x][y] = aplica_kernel_f(x, y, ancho/2, Cb420[y], (x % 2) == 0);
+        _Cr420[x][y] = aplica_kernel_f(x, y, ancho/2, Cr420[y], (x % 2) == 0);
+      }
+    }
+}
+
 
 void ConversionYCbCr420aDWT(int ancho, int alto, float **Y420, float **Cb420, float **Cr420) {
   
